32deb2d5b5028e1b2d63e98f8efabe8788b34884
popdf10 <popdf10@gmail.com>
2022-07-01 20:40:11 +0900

    #4

--- src/server.js
+++ src/server.js
@@ -1,24 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videorouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-    return res.send("<h1>I love You</h1>");
-}
-
-const handleProtected = (req, res) => {
-    return res.send("Weclome to the private lounge.");
-}
-
 app.use(logger);
 
-app.get("/", handleHome);
-app.get("protected", handleProtected);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`)
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
