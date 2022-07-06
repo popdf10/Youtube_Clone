71948310afc0bde493695070533d2b58e652fae8
popdf10 <popdf10@gmail.com>
2022-07-06 14:21:56 +0900

    #5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,38 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  res.send(`Watch video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Whatsup",
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+
+  res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
